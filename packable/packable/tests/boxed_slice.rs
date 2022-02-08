c2dbac8e839d7d9a1024a5a208176839b13a4cf5
Christian Poveda <christian.poveda@iota.org>
2022-02-08 17:33:44 -0500

    split tests

--- packable/packable/tests/boxed_slice.rs
+++ packable/packable/tests/boxed_slice.rs
@@ -4,9 +4,11 @@
 mod common;
 
 #[test]
-fn packable_vec() {
+fn packable_boxed_slice() {
     assert_eq!(
-        common::generic_test(&vec![Some(0u32), None]).0.len(),
+        common::generic_test(&vec![Some(0u32), None].into_boxed_slice())
+            .0
+            .len(),
         core::mem::size_of::<u64>()
             + (core::mem::size_of::<u8>() + core::mem::size_of::<u32>())
             + core::mem::size_of::<u8>()
