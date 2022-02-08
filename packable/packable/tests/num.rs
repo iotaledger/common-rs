93883af9125f438fe69a7b9d3fe8907be1d08f78
Christian Poveda <christian.poveda@iota.org>
2022-02-08 16:50:33 -0500

    rename test macro

--- packable/packable/tests/num.rs
+++ packable/packable/tests/num.rs
@@ -6,7 +6,7 @@ mod common;
 #[cfg(feature = "primitive-types")]
 use primitive_types::U256;
 
-macro_rules! impl_packable_test_for_integer {
+macro_rules! impl_packable_test_for_num {
     ($name:ident, $ty:ident, $value:expr) => {
         #[test]
         fn $name() {
@@ -18,20 +18,20 @@ macro_rules! impl_packable_test_for_integer {
     };
 }
 
-impl_packable_test_for_integer!(packable_i8, i8, 0x6F);
-impl_packable_test_for_integer!(packable_u8, u8, 0x6F);
-impl_packable_test_for_integer!(packable_i16, i16, 0x6F7B);
-impl_packable_test_for_integer!(packable_u16, u16, 0x6F7B);
-impl_packable_test_for_integer!(packable_i32, i32, 0x6F7BD423);
-impl_packable_test_for_integer!(packable_u32, u32, 0x6F7BD423);
-impl_packable_test_for_integer!(packable_i64, i64, 0x6F7BD423100423DB);
-impl_packable_test_for_integer!(packable_u64, u64, 0x6F7BD423100423DB);
+impl_packable_test_for_num!(packable_i8, i8, 0x6F);
+impl_packable_test_for_num!(packable_u8, u8, 0x6F);
+impl_packable_test_for_num!(packable_i16, i16, 0x6F7B);
+impl_packable_test_for_num!(packable_u16, u16, 0x6F7B);
+impl_packable_test_for_num!(packable_i32, i32, 0x6F7BD423);
+impl_packable_test_for_num!(packable_u32, u32, 0x6F7BD423);
+impl_packable_test_for_num!(packable_i64, i64, 0x6F7BD423100423DB);
+impl_packable_test_for_num!(packable_u64, u64, 0x6F7BD423100423DB);
 #[cfg(has_i128)]
-impl_packable_test_for_integer!(packable_i128, i128, 0x6F7BD423100423DBFF127B91CA0AB123);
+impl_packable_test_for_num!(packable_i128, i128, 0x6F7BD423100423DBFF127B91CA0AB123);
 #[cfg(has_u128)]
-impl_packable_test_for_integer!(packable_u128, u128, 0x6F7BD423100423DBFF127B91CA0AB123);
+impl_packable_test_for_num!(packable_u128, u128, 0x6F7BD423100423DBFF127B91CA0AB123);
 #[cfg(feature = "primitive-types")]
-impl_packable_test_for_integer!(
+impl_packable_test_for_num!(
     packable_u256,
     U256,
     U256([
@@ -41,5 +41,5 @@ impl_packable_test_for_integer!(
         0x6F7BD423100423DB
     ])
 );
-impl_packable_test_for_integer!(packable_f32, f32, core::f32::consts::PI);
-impl_packable_test_for_integer!(packable_f64, f64, core::f64::consts::PI);
+impl_packable_test_for_num!(packable_f32, f32, core::f32::consts::PI);
+impl_packable_test_for_num!(packable_f64, f64, core::f64::consts::PI);
