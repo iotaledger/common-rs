4a21508db7a4c8b08ab0c701b21ba73132a4c714
Christian Poveda <christian.poveda@iota.org>
2022-05-06 14:31:41 -0500

    add `unpack_visitor` attribute test

---
+++ packable/packable-derive-test/tests/pass/unpack_visitor.rs
@@ -0,0 +1,43 @@
+// Copyright 2021-2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#![allow(unused_imports)]
+
+use packable::{error::UnknownTagError, Packable};
+
+use core::borrow::{Borrow, BorrowMut};
+use core::convert::Infallible;
+
+pub struct Visitor {
+    inner: (),
+}
+
+impl Borrow<()> for Visitor {
+    fn borrow(&self) -> &() {
+        &self.inner
+    }
+}
+
+impl BorrowMut<()> for Visitor {
+    fn borrow_mut(&mut self) -> &mut () {
+        &mut self.inner
+    }
+}
+
+#[derive(Packable)]
+#[packable(unpack_error = Infallible)]
+#[packable(unpack_visitor = Visitor)]
+pub struct Point(i32, i32);
+
+#[derive(Packable)]
+#[packable(tag_type = u8)]
+#[packable(unpack_error = UnknownTagError<u8>)]
+#[packable(unpack_visitor = Visitor)]
+pub enum Answer {
+    #[packable(tag = 0)]
+    Yes,
+    #[packable(tag = 1)]
+    No
+}
+
+fn main() {}
